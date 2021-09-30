ef40d9ec097ef42960ecdcf1ec70ec1cff3b472f
luisMendoza00 <71712459+luisMendoza00@users.noreply.github.com>
2021-09-29 22:47:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+global.client = client;
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(process.env.TOKEN);
